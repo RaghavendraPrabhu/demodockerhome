53ff0e12b23e9579aa33c5dd517ab48b57bb2844
Raghavendra <raghavendra_uday@yahoo.com>
2018-09-23 18:29:45 +0530

    POM updated pom

--- src/main/java/demodocker4/App.java
+++ src/main/java/demodocker4/App.java
@@ -8,6 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( " Ganpati Bappa Morya !!! " );
+        System.out.println( " Ganpati Bappa Morya !!! Come Again Bappa " );
     }
 }
