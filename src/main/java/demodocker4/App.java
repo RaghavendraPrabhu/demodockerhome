61f4ed6367e8ee86a291891cd4d63f8349b86a67
Raghavendra <raghavendra_uday@yahoo.com>
2018-09-23 20:26:26 +0530

    Updated the image name

--- src/main/java/demodocker4/App.java
+++ src/main/java/demodocker4/App.java
@@ -8,6 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( " Ganpati Bappa Morya !!! Come Again Bappa !!! " );
+        System.out.println( " Ganpati Bappa Morya !!! Come Again Bappa !!! I Love U " );
     }
 }
